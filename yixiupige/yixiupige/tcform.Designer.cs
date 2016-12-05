ecbf17996051974f49eaac52d2dec3df0e4df1c0
wryip <15650158517@163.com>
2016-12-05 21:36:03 +0800

    添加了按钮

--- yixiupige/yixiupige/tcform.Designer.cs
+++ yixiupige/yixiupige/tcform.Designer.cs
@@ -28,11 +28,32 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(39, 52);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // tcform
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "tcform";
+            this.ClientSize = new System.Drawing.Size(684, 353);
+            this.Controls.Add(this.button1);
+            this.Name = "tcform";
+            this.Text = "提成管理";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
     }
 }
