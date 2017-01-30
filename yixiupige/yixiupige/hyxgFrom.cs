1a512c6df6bce7d0f7fa737cafe87011149a18bf
wryip <15650158517@163.com>
2017-01-30 11:31:09 +0800

    1.30提交

---
+++ yixiupige/yixiupige/hyxgFrom.cs
@@ -0,0 +1,88 @@
+﻿using BLL;
+using MODEL;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace yixiupige
+{
+    public partial class hyxgFrom : Form
+    {
+        public hyxgFrom()
+        {
+            InitializeComponent();
+        }
+        public jbcsBLL jbbll = new jbcsBLL();
+        staffInfoBLL staffbll = new staffInfoBLL();
+        public static staffTable model;
+        private static hyxgFrom yggl;
+        public delegate void databind();
+        public static databind bind;
+        public static hyxgFrom Create(staffTable model11,databind bind1)
+        {
+            bind = bind1;
+            model = model11;
+            if (yggl == null)
+            {
+                yggl = new hyxgFrom();
+            }
+            return yggl;
+        }
+        private void hyxgFrom_Load(object sender, EventArgs e)
+        {
+            List<jbcs> list = jbbll.selectList(6);
+            foreach (var iteam in list)
+            {
+                zwgzcomboBox.Items.Add(iteam.AllType);
+            }
+            ygxmtextBox.Text = model.stName;
+            ygxbcomboBox.Text = model.stSex;
+            zwgzcomboBox.Text = model.stType;
+            sfzhtextBox.Text = model.stDocument;
+            lxdhtextBox.Text = model.stTel;
+            jtzztextBox.Text = model.stAdd;
+            bzxxtextBox.Text = model.stRemark;
+        }
+
+        private void hyxgFrom_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            yggl = null;
+        }
+
+        private void tcbutton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void zjbutton_Click(object sender, EventArgs e)
+        {
+            staffTable newmodel = new staffTable();
+            newmodel.Id = model.Id;
+            newmodel.stName = ygxmtextBox.Text.Trim();
+            newmodel.stType = zwgzcomboBox.Text.Trim();
+            newmodel.stSex = ygxbcomboBox.Text.Trim();
+            newmodel.stDocument = sfzhtextBox.Text.Trim() == "" ? "0" : sfzhtextBox.Text.Trim();
+            newmodel.stTel = lxdhtextBox.Text.Trim() == "" ? "0" : lxdhtextBox.Text.Trim();
+            newmodel.stAdd = jtzztextBox.Text.Trim() == "" ? "0" : jtzztextBox.Text.Trim();
+            newmodel.stRemark = bzxxtextBox.Text.Trim() == "" ? "0" : bzxxtextBox.Text.Trim();
+            bool result = staffbll.updateModel(newmodel);
+            if (result)
+            {
+                MessageBox.Show("修改成功！");
+                bind();
+                this.Close();
+
+            }
+            else 
+            {
+                MessageBox.Show("修改失败！");
+            }
+        }
+    }
+}
---
+++ yixiupige/yixiupige/jbfuCheckBoxFrom.cs
@@ -0,0 +1,110 @@
+﻿using BLL;
+using MODEL;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace yixiupige
+{
+    public partial class jbfuCheckBoxFrom : Form
+    {
+        public jbfuCheckBoxFrom()
+        {
+            InitializeComponent();
+        }
+        fuwuBLL fuwubl = new fuwuBLL();
+        public delegate void TiJiao(string model);
+        public static TiJiao action;
+        private static jbfuCheckBoxFrom _danli = null;
+        public static jbfuCheckBoxFrom CreateForm(TiJiao action1)
+        {
+            action = action1;
+            if (_danli == null)
+            {
+                _danli = new jbfuCheckBoxFrom();
+            }
+            return _danli;
+        }
+        private void jbfuCheckBoxFrom_Load(object sender, EventArgs e)
+        {
+            List<fuwuModel> list = new List<fuwuModel>();
+            list = fuwubl.selectAllList();
+            tableLayoutPanel1.Height = (list.Count + 1) * 40;
+            //double rows = Math.Ceiling(list.Count*1.0/4);
+            //int row = Convert.ToInt32(rows);
+            CheckBox chek;
+            RowStyle rowsty = new RowStyle(SizeType.Absolute, 40); ;
+            //for (int i = 0; i < row; i++)
+            //{
+                int jishu=0;               
+                foreach (var iteam in list)
+                {
+                    chek = new CheckBox();
+                    chek.Text = iteam.Name;
+                    rowsty = new RowStyle(SizeType.Absolute, 40);
+                    tableLayoutPanel1.Controls.Add(chek, jishu, tableLayoutPanel1.RowStyles.Count - 1);
+                    jishu++;
+                    if (jishu == 4)
+                    {
+                        tableLayoutPanel1.RowStyles.Add(rowsty);
+                        jishu = 0;
+                    }
+                }                
+            //}
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void jbfuCheckBoxFrom_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            _danli = null;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            hyflglfwForm hyflgl = hyflglfwForm.Create();
+            hyflgl.Show();
+            hyflgl.Focus();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //tb = tableLayoutPanel1.GetControlFromPosition(1, i) as TextBox;
+            int rows = tableLayoutPanel1.RowStyles.Count;
+            CheckBox check;
+            string name = "";
+            for (int i = 0; i < rows; i++)
+            {
+                //int jishu = 0;
+                for (int j = 0; i < 4; j++)
+                {
+                    check = tableLayoutPanel1.GetControlFromPosition(j, i) as CheckBox;
+                    if (check == null)
+                    {
+                        break;
+                    }
+                    if (check.Checked == true)
+                    {
+                        name += check.Text.Trim() + ",";
+                    }  
+                }
+            }
+            if (name == "")
+            {
+                MessageBox.Show("请选择服务！");
+                return;
+            }
+            action(name);
+            this.Close();
+        }
+    }
+}
